73849adcefd2eb85de6394d99936695738c98b01
RamekhCHHOENG <ramekhchhoeng@icloud.com>
2023-04-05 12:59:41 +0700

    feat: update routing

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,85 +1,88 @@
-import express, { Request, Response } from 'express';
-import { Types } from 'mongoose';
-import { v4 as uuidv4 } from 'uuid';
-import Todo, { ITodo } from '../models/Todo';
+import express, { Request, Response } from 'express'
+import { Types } from 'mongoose'
+import { v4 as uuidv4 } from 'uuid'
+import Todo, { ITodo } from '../models/Todo'
 
-const router = express.Router();
+const router = express.Router()
 
 router.get('/todos', async (req: Request, res: Response) => {
   try {
-    const { q } = req.query;
-    const filter = q ? { todo: { $regex: q, $options: 'i' } } : {};
-    const todos = await Todo.find(filter).sort({createdAt: -1});
-    res.json({ success: true, data: todos });
+    const { q } = req.query
+    const filter = q ? { todo: { $regex: q, $options: 'i' } } : {}
+    const todos = await Todo.find(filter)
+      .select('id todo isCompleted createdAt updatedAt')
+      .sort({ createdAt: -1 })
+    res.json({ success: true, data: todos })
   } catch (err: any) {
-    res.status(500).json({ success: false, error: err.message });
+    res.status(500).json({ success: false, error: err.message })
   }
-});
+})
 
 router.post('/todo', async (req: Request, res: Response) => {
   try {
-    const { todo } = req.body;
+    const { todo } = req.body
 
-    const existingTodo = await Todo.findOne({ todo });
+    const existingTodo = await Todo.findOne({ todo })
     if (existingTodo) {
-      return res.status(400).json({ success: false, error: 'Todo already exists' });
+      return res
+        .status(400)
+        .json({ success: false, error: 'Todo already exists' })
     }
     const newTodo: ITodo = new Todo({
       id: uuidv4(),
       todo,
-      createdAt: Date.now(),
-    });
+      createdAt: Date.now()
+    })
 
-    await newTodo.save();
+    await newTodo.save()
 
-    res.json({ success: true, data: newTodo });
+    res.json({ success: true, data: newTodo })
   } catch (err: any) {
-    res.status(500).json({ success: false, error: err.message });
+    res.status(500).json({ success: false, error: err.message })
   }
-});
+})
 
 router.put('/todo/:id', async (req: Request, res: Response) => {
   try {
-    const { id } = req.params;
+    const { id } = req.params
 
-    const updatedFields: any = {};
-    if (req.body.todo) updatedFields.todo = req.body.todo;
-    if (req.body.isCompleted !== undefined) updatedFields.isCompleted = req.body.isCompleted;
+    const updatedFields: any = {}
+    if (req.body.todo) updatedFields.todo = req.body.todo
+    if (req.body.isCompleted !== undefined)
+      updatedFields.isCompleted = req.body.isCompleted
 
-    updatedFields.updatedAt = Date.now();
+    updatedFields.updatedAt = Date.now()
 
-    const updatedTodo = await Todo.findByIdAndUpdate(id,
-      { $set: updatedFields },
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      id,
+      updatedFields,
       { new: true }
-    );
+    )
 
-    res.json({ success: true, data: updatedTodo });
+    res.json({ success: true, data: updatedTodo })
   } catch (err: any) {
-    res.status(500).json({ success: false, error: err.message });
+    res.status(500).json({ success: false, error: err.message })
   }
-});
-
+})
 
 router.delete('/todo/:id', async (req: Request, res: Response) => {
   try {
-    const { id } = req.params;
+    const { id } = req.params
 
     if (!Types.ObjectId.isValid(id)) {
-      return res
-        .status(404)
-        .json({ success: false, error: 'Invalid todo ID' });
+      return res.status(404).json({ success: false, error: 'Invalid todo ID' })
     }
 
-    const todo = await Todo.findByIdAndDelete(id);
+    const todo = await Todo.findByIdAndDelete(id)
 
     if (!todo) {
-      return res.status(404).json({ success: false, error: 'Todo not found' });
+      return res.status(404).json({ success: false, error: 'Todo not found' })
     }
 
-    res.json({ success: true, data: todo });
+    res.json({ success: true, data: todo })
   } catch (err: any) {
-    res.status(500).json({ success: false, error: err.message });
+    res.status(500).json({ success: false, error: err.message })
   }
-});
+})
 
-export default router;
+export default router
